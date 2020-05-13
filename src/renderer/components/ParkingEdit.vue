6e87c3428b90d0c124dd12caa6dc115156cd57e1
portree_kid <keith.paterson@gmx.de>
2020-05-13 15:14:14 +0200

    Styling and radii

--- src/renderer/components/ParkingEdit.vue
+++ src/renderer/components/ParkingEdit.vue
@@ -13,23 +13,32 @@
     -->
     <el-row>
       <el-col :span="4">
-        <span class="demo-input-label">Name :</span>
+        <span class="label">Name :</span>
       </el-col>
       <el-col :span="8">
         <el-input placeholder="Name" v-model="name"></el-input>
       </el-col>
-      <el-col :span="4">
-        <span class="demo-input-label">Number :</span>
+      <el-col :span="5">
+        <span class="label">Number :</span>
       </el-col>
-      <el-col :span="8">
+      <el-col :span="7">
         <el-input placeholder="Number" v-model="number"></el-input>
       </el-col>
     </el-row>
     <el-row>
       <el-col :span="7">
-        <span class="demo-input-label">Size :</span>
+        <span class="label">Size :</span>
       </el-col>
       <el-col :span="17">
+        <!--
+          * Cat Models                   FG Radii N2M Radii
+          * B Small Regionals ERJ CRJ ATR    14       6
+          * C A319 A320 A321 B737            18      10
+          * D B757, B767                     26      15
+          * E B777 B787 A330 A340 A360       33      24
+          * F A380                           40      24
+          -->
+
         <el-radio-group v-model="wingspan">
           <el-tooltip content="PIPER PA-31/CESSNA 404 Titan" placement="top" effect="light">
             <el-radio :label="15">A</el-radio>
@@ -39,7 +48,7 @@
             placement="top"
             effect="light"
           >
-            <el-radio :label="24">B</el-radio>
+            <el-radio :label="28">B</el-radio>
           </el-tooltip>
           <el-tooltip
             content="BOEING 737-700/AIRBUS A-320/EMBRAER ERJ 190-100"
@@ -52,7 +61,7 @@
             <el-radio :label="52">D</el-radio>
           </el-tooltip>
           <el-tooltip content="B777 Series/B787 Series/A330 Family" placement="top" effect="light">
-            <el-radio :label="65">E</el-radio>
+            <el-radio :label="66">E</el-radio>
           </el-tooltip>
           <el-tooltip content="BOEING 747-8/AIRBUS A-380-800" placement="top" effect="light">
             <el-radio :label="80">F</el-radio>
@@ -62,13 +71,13 @@
     </el-row>
     <el-row>
       <el-col :span="7">
-        <span class="demo-input-label">Aircraft :</span>
+        <span class="label">Aircraft :</span>
       </el-col>
       <el-col :span="17">{{type}}</el-col>
     </el-row>
     <el-row>
       <el-col :span="7">
-        <span class="demo-input-label">Coordinates :</span>
+        <span class="label">Coordinates :</span>
       </el-col>
       <el-col :span="17">
         <el-input placeholder="Please input" v-model="coordinates"></el-input>
@@ -76,7 +85,7 @@
     </el-row>
     <el-row>
       <el-col :span="7">
-        <span class="demo-input-label">Parking Type :</span>
+        <span class="label">Parking Type :</span>
       </el-col>
       <el-col :span="17">
         <el-select v-model="parking_type" placeholder="Select">
@@ -91,7 +100,7 @@
     </el-row>
     <el-row>
       <el-col :span="7">
-        <span class="demo-input-label">Airline :</span>
+        <span class="label">Airline :</span>
       </el-col>
       <el-col :span="17">
         <el-select v-model="airlineCodes" multiple placeholder="Select">
@@ -106,7 +115,7 @@
     </el-row>
     <el-row>
       <el-col :span="7">
-        <span class="demo-input-label">Pushback Route End :</span>
+        <span class="label">Pushback Route End :</span>
       </el-col>
       <el-col :span="17">{{pushbackEnd}}</el-col>
     </el-row>
@@ -207,16 +216,25 @@
         return this.$parent.$parent.$parent.$refs.editLayer.findRouteToPushback(this.$store.state.Editable.index)
       },
       type: function () {
+/**
+ * Cat Models                   FG Radii N2M Radii
+ * B Small Regionals ERJ CRJ ATR    14       6
+ * C A319 A320 A321 B737            18      10
+ * D B757, B767                     26      15
+ * E B777 B787 A330 A340 A360       33      24
+ * F A380                           40      24
+ */
+
         switch (this.$store.state.Editable.data.parking.radius * 2) {
           case 15:
             return 'PIPER PA-31/CESSNA 404 Titan'
-          case 24:
+          case 28:
             return 'BOMBARDIER Regional Jet CRJ-200/DE HAVILLAND CANADA DHC-6'
           case 36:
             return 'BOEING 737-700/AIRBUS A-320/EMBRAER ERJ 190-100'
           case 52:
             return 'B767 Series/AIRBUS A-310'
-          case 65:
+          case 66:
             return 'B777 Series/B787 Series/A330 Family'
           case 80:
             return 'BOEING 747-8/AIRBUS A-380-800'
@@ -257,6 +275,12 @@
   margin: 1px;
 }
 .el-col {
-  padding-left: 10pt;
+  padding-left: 2pt;
+}
+.label {
+display: flex;
+  justify-content: left;
+  align-items: center;
+  font-weight: bold;
 }
 </style>
