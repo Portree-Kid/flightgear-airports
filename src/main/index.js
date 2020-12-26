5c01f2a656feae367300cda45c045cb89b358422
Keith Paterson <keith.paterson@gmx.de>
2020-12-26 20:50:55 +0100

    Remove Menu

--- src/main/index.js
+++ src/main/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import { app, BrowserWindow } from 'electron'
+import { app, BrowserWindow, Menu } from 'electron'
 
 const { ipcMain } = require('electron')
 ipcMain.on('OpenDebugger', (event, arg) => {
@@ -20,6 +20,7 @@ const winURL = process.env.NODE_ENV === 'development'
   : `file://${__dirname}/index.html`
 
 function createWindow () {
+  Menu.setApplicationMenu(null)
   /**
    * Initial window options
    */
